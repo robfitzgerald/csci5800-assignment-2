cfd2a08e7ce56e73e395536cd4d8fa6e8dd28461
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-02-15 20:53:48 -0700

    assignment completed.

--- goats.rkt
+++ goats.rkt
@@ -1,98 +1,151 @@
 #lang racket
 
+; goats.rkt
+; graph search program for the Mountain Goat Dance Party Problem
+
 ; rob fitzgerald
 ; s# 103400432
 ; csci5800 spring 2016
 
 (require "graph-search.rkt")
 
-;;; csci 5800 assignment 2: Black and White Mountain Goat Dance Party
+; see a2-writeup.txt for written responses to questions in the assignment.
 
-;;;  problem statement
- 
-; Three black mountain goats one direction on a narrow mountain ledge and encounter three white mountain goats 
-; going in the opposite direction. They stop with the space of a goat between the two lead animals. Mountain goats 
-; cannot (for the purposes of the puzzle) move backwards, but they are good jumpers. A mountain goat can either 
-; move forward into an empty space, jump over one mountain goat to an empty space, or jump over two mountain goats 
-; to an empty space. How can they all continue on their way?
 
-; We can abstract this to the following: B B B _ W W W
+;;;;;;;;;;;;;;;;;;
+;;; containers ;;;
+;;;;;;;;;;;;;;;;;;
 
-; Where each B represents a black mountain goat, each W represents a white mountain goat, and _ represents a blank 
-; space. Each B may move right into an adjacent _, jump over an intervening B or W into an _, or jump over two 
-; intervening B or W into an _. Each W may similarly move left the same way. The goal is to get all of the Bs to 
-; the right of all of the Ws.
-
-; Use the graph search program provided in class to build a state space solution to the problem. Is there a 
-; difference in the solution for a breadth-first versus depth-first traversal of the graph? What is the difference 
-; in the number of node generated and explored?
-
-; Extend your representation to handle any number of black and white goats – they don’t have to be the same number.
-; Is the problem solvable for all such combinations?
-
-;;; containers
-
+;;; note: over-writing built-in keyword _ used for "match any" syntax object.
 (define _ '_)
 (define B 'B)
 (define W 'W)
-(define (new-state p0 p1 p2 p3 p4 p5 p6)
-  (vector p0 p1 p2 p3 p4 p5 p6))
+
+;;; (new-state . pieces) -> vector?
+;;;   pieces: list?
+;;; state space arguments for beginning the graph search
+(define (new-state . pieces)
+  (list->vector pieces))
+
+;;; (copy-state state) -> vector?
+;;;   state: vector?
+;;; creates a copy in memory of the provided vector
 (define (copy-state state)
   (vector-copy state))
-;define state as vector.
-  
-;;; predicates
-  
+
+;;;;;;;;;;;;;;;;;;  
+;;; predicates ;;;
+;;;;;;;;;;;;;;;;;;
+
+;;; (_? a) -> boolean?
+;;;   a: any?
+;;; does the value match the symbol for the empty space
 (define (_? a)
   (eq? a '_))
+
+;;; (B? a) -> boolean?
+;;;   a: any?
+;;; does the value match the symbol for the black goat
 (define (B? a)
   (eq? a 'B))
+
+;;; (W? a) -> boolean?
+;;;   any?
+;;; does the value match the symbol for the white goat
 (define (W? a)
   (eq? a 'W))
-(define (in-range? num)
+
+;;; (in-range? num limit) -> boolean?
+;;;   num: number?
+;;;   limit: number?
+;;; bounds check for the vector containing the search state
+(define (in-range? num limit)
   (and (number? num)
        (>= num 0)
-       (< num 7)))
+       (< num limit)))
 
-;;; queries & query helpers.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; queries & query helpers. ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; (state-length state) => number?
+;;;   state: vector?
+;;; alias for vector-length: count of elements in the search space
+(define (state-length state)
+  (vector-length state))
 
 ;;; (at state pos) => any/c
+;;;   state: vector?
+;;;   pos: number?
+;;; alias for vector-ref,
+;;; returns the symbol that is stored at the position pos in the vector state.
 (define (at state pos)
   (vector-ref state pos))
 
 ;;; (at-space state) => number?
+;;;   state: vector?
+;;; returns the position of the first (of what should only be one) 
+;;; underscore character in the state space
 (define (at-space state)
   (vector-member _ state))
 
+;;; these six functions determine index values for adjacent and near-adjacent vector indices
+
+;;; (left-of num) -> number?
+;;;   num: number?
 (define (left-of num)
   (- num 1))
+
+;;; (two-left-of num) -> number?
+;;;   num: number?
 (define (two-left-of num)
   (- num 2))
+
+;;; (three-left-of num) -> number?
+;;;   num: number?
 (define (three-left-of num)
   (- num 3))
+
+;;; (right-of num) -> number?
+;;;   num: number?
 (define (right-of num)
   (+ num 1))
+
+;;; (two-right-of num) -> number?
+;;;   num: number?
 (define (two-right-of num)
   (+ num 2))
+
+;;; (three-right-of num) -> number?
+;;;   num: number?
 (define (three-right-of num)
   (+ num 3))
 
-;;; mutators
+;;;;;;;;;;;;;;;;
+;;; mutators ;;;
+;;;;;;;;;;;;;;;;
 
+;;; (swap vec i j) -> vector?
+;;;   vec: vector?
+;;;   i: number?
+;;;   j: number?
+;;; traditional vector swap function.
 (define (swap vec i j)
   (define temp (vector-ref vec i))
   (vector-set! vec i (vector-ref vec j))
   (vector-set! vec j temp)
   vec)
   
-;;; rules
+;;;;;;;;;;;;;
+;;; rules ;;;
+;;;;;;;;;;;;;
 
 (define-ruleset goats-rule)
 
 ; B _
 (define-rule goats-rule (B-step-1 state)
-  (and (in-range? (left-of (at-space state)))
-       (in-range? (at-space state))
+  (and (in-range? (left-of (at-space state)) (state-length state))
+       (in-range? (at-space state) (state-length state))
        (B? (at state (left-of (at-space state)))))
        
 =>
@@ -100,45 +153,45 @@
 
 ; _ W
 (define-rule goats-rule (W-step-1 state)
-  (and (in-range? (right-of (at-space state)))
-       (in-range? (at-space state))
+  (and (in-range? (right-of (at-space state)) (state-length state))
+       (in-range? (at-space state) (state-length state))
        (W? (at state (right-of (at-space state)))))
 =>
   (swap (copy-state state) (at-space state) (right-of (at-space state))))
 
 ; B * _
 (define-rule goats-rule (B-jump-1 state)
-  (and (in-range? (two-left-of (at-space state)))
-       (in-range? (at-space state))
+  (and (in-range? (two-left-of (at-space state)) (state-length state))
+       (in-range? (at-space state) (state-length state))
        (B? (at state (two-left-of (at-space state)))))
 =>
   (swap (copy-state state) (at-space state) (two-left-of (at-space state))))
 
 ; _ * W
 (define-rule goats-rule (W-jump-1 state)
-  (and (in-range? (two-right-of (at-space state)))
-       (in-range? (at-space state))
+  (and (in-range? (two-right-of (at-space state)) (state-length state))
+       (in-range? (at-space state) (state-length state))
        (W? (at state (two-right-of (at-space state)))))
 =>
   (swap (copy-state state) (at-space state) (two-right-of (at-space state))))
 
 ; B * * _
 (define-rule goats-rule (B-jump-2 state)
-  (and (in-range? (three-left-of (at-space state)))
-       (in-range? (at-space state))
+  (and (in-range? (three-left-of (at-space state)) (state-length state))
+       (in-range? (at-space state) (state-length state))
        (B? (at state (three-left-of (at-space state)))))
 =>
   (swap (copy-state state) (at-space state) (three-left-of (at-space state))))
 
 ; _ * * W
 (define-rule goats-rule (W-jump-2 state)
-  (and (in-range? (three-right-of (at-space state)))
-       (in-range? (at-space state))
+  (and (in-range? (three-right-of (at-space state)) (state-length state))
+       (in-range? (at-space state) (state-length state))
        (W? (at state (three-right-of (at-space state)))))
 =>
   (swap (copy-state state) (at-space state) (three-right-of (at-space state))))
 
-;;; search
+;;; graph-search interface / runner
 
 (define (goats-search initial-state goal-state merge-method)
   (printf "(goats-search ~a ~a '~a)~n" initial-state goal-state merge-method)
@@ -157,5 +210,11 @@
           (printf "No solution found.~n")))
         (printf "---~n~n"))
 
+
+
 (goats-search (new-state B B B _ W W W) (new-state W W W _ B B B) 'prepend)
 (goats-search (new-state B B B _ W W W) (new-state W W W _ B B B) 'append)
+
+; solves for any size state. does not need to be symmetrical, but 
+; all goats of each type must be on either side of the space (as in the problem description).
+;(goats-search (new-state B B B B B B B B B B _ W W W W) (new-state W W W W _ B B B B B B B B B B) 'append)
